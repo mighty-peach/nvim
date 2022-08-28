7cac38d15675622dd138324c82021438cba3661d
mighty_peach <mighty_peach@fastmail.com>
2022-08-29 00:03:41 +0700

    add tasks

--- init.lua
+++ init.lua
@@ -3,3 +3,9 @@ require('highlights')
 require('macos')
 require('maps')
 require('plugins')
+
+-- how to open defenition via gd
+-- colorize tabs (topbar)
+-- colorize tabs and dangling whitespaces
+-- how to navigate between buffers??
+
