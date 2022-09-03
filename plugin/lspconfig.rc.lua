3286994e48f1754a53265170cada981b83f3e2c6
mighty_peach <mighty_peach@fastmail.com>
2022-09-03 21:12:41 +0700

    add rust lsp

--- plugin/lspconfig.rc.lua
+++ plugin/lspconfig.rc.lua
@@ -71,6 +71,15 @@ nvim_lsp.tsserver.setup {
   capabilities = capabilities
 }
 
+nvim_lsp.rls.setup {
+  settings = {
+    rust = {
+      unstable_features = false,
+      build_on_save = false,
+      all_features = true,
+    },
+  },
+}
 
 vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
   vim.lsp.diagnostic.on_publish_diagnostics, {
