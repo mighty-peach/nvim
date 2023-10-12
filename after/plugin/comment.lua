75a598b8e1e9a53741600d06dfb94b562dfe7556
mighty_peach <mighty_peach@fastmail.com>
2023-10-12 09:24:25 +0200

    update telescope

--- after/plugin/comment.lua
+++ after/plugin/comment.lua
@@ -1,15 +1,15 @@
 require('Comment').setup({
   toggler = {
     ---Line-comment toggle keymap
-    line = 'gcc',
+    line = '<leader>gc',
     ---Block-comment toggle keymap
-    block = 'gC',
+    block = '<leader>gC',
   },
   ---LHS of operator-pending mappings in NORMAL and VISUAL mode
   opleader = {
     ---Line-comment keymap
-    line = 'gcl',
+    line = '<leader>gc',
     ---Block-comment keymap
-    block = 'gcb',
+    block = '<leader>gC',
   },
 })
